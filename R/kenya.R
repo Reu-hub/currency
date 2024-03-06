b9b5ef00eec5790639442358458237137e1456f4
opondoreuben94@gmail.com <opondoreuben94@gmail.com>
2024-03-07 00:36:16 +0300

    updates

--- R/kenya.R
+++ R/kenya.R
@@ -20,7 +20,8 @@ kshs_usd <- function(){
   kshs_usd_data_raw = read_csv(file = kshs_usd_historic_url) %>% 
     mutate(currency = read_html(x = kshs_usd_url) %>% html_node("h1") %>% html_text()) %>% 
     clean_names() %>% 
-    arrange(desc(date))
+    arrange(desc(date)) %>% 
+    mutate(across(c("open", "high", "low", "close", "adj_close", "volume"),as.numeric))
   # export data
   write_csv(x = kshs_usd_data_raw, file = "data_raw/kshs_usd_data_raw.csv")
   write_rds(x = kshs_usd_data_raw, file = "data_raw/kshs_usd_data_raw.rds", compress = "xz")
